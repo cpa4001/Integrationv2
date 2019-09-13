7891e9f3aa9e8d38d62465f30b1e62511af3dee0
cpa4001 <54412557+cpa4001@users.noreply.github.com>
2019-09-13 09:53:23 -0400

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,14 @@
+# Christian Apostoli
+# This will  be a trivia quiz which has
+# a main theme based on JOJO's Bizzare Adventures.
+# The quiz will  eventually have different question sets, and
+# be more complex.
+
+print("Hello User")
+startingScore = 100 / 100
+print(" All new players will  start out with a score of", (startingScore * 100))
+print(" A players final score will be determined by number of questions correct divided by total questions")
+numCorrect = 100
+numQuestions = 100
+scoreFinal = numCorrect / numQuestions
+print("For example, a perfect score will result in a", (int(scoreFinal) * 100))
